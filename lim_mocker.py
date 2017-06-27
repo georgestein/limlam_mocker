f96a71b6cc49a54a860cf64c9cd7dcf18d848b09
Dongwoo Chung <dongwooc@users.noreply.github.com>
2017-06-27 06:51:40 -0700

    streamlining params and modules

--- lim_mocker.py
+++ lim_mocker.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 from __future__ import division
-from limlam_mocker.mods import *
+import numpy              as np
+import matplotlib.pylab   as plt
+import scipy              as sp
+import limlam_mocker      as llm
+#Get Parameters for run
+import params             as params
 
+plt.rcParams['font.size'] = 16
+llm.write_time('Starting Line Intensity Mapper')
 
 ### Load halos from catalogue
-halos     = load_peakpatch_catalogue(params.halo_catalogue_file)
-halos     = cull_peakpatch_catalogue(halos, params.min_mass)
+halos     = llm.load_peakpatch_catalogue(params.halo_catalogue_file)
+halos     = llm.cull_peakpatch_catalogue(halos, params.min_mass)
 
 
 ### Calculate line freq from redshift
@@ -13,38 +20,43 @@ halos.nu  = params.nu_rest/(halos.redshift+1)
 
 
 ### Calculate Luminosity of each halo
-halos.Lco = Mhalo_to_Lco(halos, params.model, params.scatter)
+halos.Lco = llm.Mhalo_to_Lco(halos, params.model, params.scatter)
 
 
+#SETUP MAPS TO OUTPUT
+map       = llm.params_to_mapinst(params);
 ### Bin halo luminosities into map
-map.maps  = Lco_to_map(halos,map)
+map.maps  = llm.Lco_to_map(halos,map)
 
 ### Output map to file
-save_maps(map)
+llm.save_maps(map)
 
 ### Calculate power spectrum
 ## if the catalogue contains halo comoving distances and redshifts ...
 ##     we don't need astropy!
 from scipy.interpolate import UnivariateSpline
 redshift_to_chi = UnivariateSpline(halos.redshift,halos.chi)
-k,Pk,Pk_sampleerr = map_to_pspec(map,redshift_to_chi,params.nu_rest)
+k,Pk,Pk_sampleerr = llm.map_to_pspec(map,redshift_to_chi,params.nu_rest)
 
-### Plot first frequency map
+### Plot central frequency map
 if params.plot_cube:
     plt.figure()
     im = plt.imshow(np.log10(map.maps[:,:,params.nmaps//2]+1e-1), extent=[-map.fov_x/2,map.fov_x/2,-map.fov_y/2,map.fov_y/2])
     plt.colorbar(im,label=r'$log_{10}\ T_b\ [\mu K]$')
     plt.xlabel('degrees',fontsize=20)
     plt.ylabel('degrees',fontsize=20)
+    plt.title('simulated map at {0:.3f} GHz'.format(map.nu_bincents[params.nmaps//2]),fontsize=24)
 
 if params.plot_pspec:
     plt.figure()
     plt.errorbar(k,k**3*Pk/(2*np.pi**2),k**3*Pk_sampleerr/(2*np.pi**2),
-                    capsize=0)
+                    lw=3,capsize=0)
     plt.gca().set_xscale('log')
     plt.gca().set_yscale('log')
-    plt.xlabel('k (1/Mpc)',fontsize=20)
-    plt.ylabel('$\\Delta^2(k)$ ($\\mu$K$^2$)',fontsize=20)
+    plt.grid(True)
+    plt.xlabel('k [1/Mpc]',fontsize=18)
+    plt.ylabel('$\\Delta^2(k)$ [$\\mu$K$^2$]',fontsize=18)
+    plt.title('simulated line power spectrum',fontsize=24)
 
 if params.plot_cube or params.plot_pspec:
     plt.show()
---
+++ limlam_mocker/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from   .tools               import *
+from   .load_halos          import *
+from   .halos_to_luminosity import *
+from   .luminosity_to_map   import *
+from   .map_to_pspec        import *
